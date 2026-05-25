0544803033d83ef6c89ddbe9c20f1095529282a1
anjali <anjali8271885227@gmail.com>
2026-05-25 20:33:36 +0530

    connected frontend with render backend

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,4 +1,4 @@
-const API_BASE_URL = "http://127.0.0.1:8000";
+const API_BASE_URL = "https://phishwall-full-stack.onrender.com";
 
 export const predictMessage = async (message) => {
   const response = await fetch(`${API_BASE_URL}/predict/message`, {
